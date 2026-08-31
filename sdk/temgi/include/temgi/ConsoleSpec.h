1349c7fde44ce7904d88ff6d58327c28b0528906
driconmax <driconmax@gmail.com>
2026-08-31 21:51:01 +0900

    Update to 569x320 (16:9) console resolution

--- sdk/temgi/include/temgi/ConsoleSpec.h
+++ sdk/temgi/include/temgi/ConsoleSpec.h
@@ -6,7 +6,7 @@ namespace temgi {
 	struct ConsoleSpec {
 		static constexpr std::size_t FAST_RAM_SIZE			= 32 * 1024;
 		static constexpr std::size_t MAIN_RAM_SIZE			= 8 * 1024 * 1024;
-		static constexpr std::size_t VIDEO_RAM_SIZE			= 96 * 1024;
+		static constexpr std::size_t VIDEO_RAM_SIZE			= 256 * 1024;
 		static constexpr std::size_t SPRITE_RAM_SIZE		= 1 * 1024;
 		static constexpr std::size_t PALETTE_RAM_SIZE		= 1 * 1024;
 		static constexpr std::size_t SAVE_RAM_SIZE			= 1 * 1024;
@@ -15,7 +15,7 @@ namespace temgi {
 
 		static constexpr std::size_t MAX_CARTRIDGE_SIZE		= 32 * 1024 * 1024;
 
-		static constexpr int SCREEN_WIDTH		= 256;
-		static constexpr int SCREEN_HEIGHT		= 240;
+		static constexpr int SCREEN_WIDTH		= 569;
+		static constexpr int SCREEN_HEIGHT		= 320;
 	};
 }
